8c75169804bfda614ede3ac9a83eb6bd92955471
chaLyoung <dreamlovinghigh57@gmail.com>
2022-10-24 19:20:07 +0900

    remove MainService Autowired-MainController

--- src/main/java/com/dev/springboot_basic/service/MainService.java
+++ src/main/java/com/dev/springboot_basic/service/MainService.java
@@ -1,14 +1,10 @@
 package com.dev.springboot_basic.service;
 
-import com.dev.springboot_basic.controller.MainController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainService {
-    @Autowired
-    private MainController mainController;
-
     public String getMessage(String msg) {
 
         return "return " + msg + "by mainService";
